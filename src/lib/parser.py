e531c991952346f5f1abaa62b2d9d1124502e598
Simon Thelen <urday@student.kit.edu>
2015-01-21 03:35:54 +0100

    lib/parser: implement wparse
    Parses the file containing the SUMO to WordNet mapping and returns a
    dictionary mapping entities in the Ontology to WordNet synsets.

--- src/lib/parser.py
+++ src/lib/parser.py
@@ -10,7 +10,10 @@ This module contains:
 
 """
 
+import re
+
 from os.path import basename
+from enum import Enum
 
 def kifparse(ontology, graph=None, ast=None):
     """ Parse an ontology and return an AbstractSyntaxTree.
@@ -58,19 +61,120 @@ def kifserialize(ast, ontology):
     """
     pass
 
-def wparse(mapping):
+def wparse(path):
     """ Parses the file containing the SUMO-WordNet mapping.
 
     Args:
 
-    - mapping: The file for the SUMO WordNet mapping
+    - path: The path to the SUMO-WordNet mapping files
 
     Returns:
 
     - Dictionary
 
     """
-    pass
+    mapping = dict()
+    total, processed = 0, 0
+    for pos in Pos:
+        with open('%s/wordnet/sdata.%s' % (path, pos.name)) as data:
+            for line in data:
+                total += 1
+                #Syntactical validation
+                if re.match(r'^(\d{8}) (\d{2}) ([nvasr]) ([0-9a-zA-Z]{2})(?: (\S+ ([0-9a-zA-Z])))+ (\d{3})(?: ((\S{1,2}) \d{8} [nvasr] [0-9a-zA-Z]{4}))*(?: \d{2} (\+ \d{2} [0-9a-zA-Z]{2} )+)? ?\| .+ &%.+[\][@+:=]$', line):
+                    items = _wtokenize(line.rstrip('\n'), pos)
+                    processed += 1
+                    for item in items:
+                        try:
+                            mapping[item.sumo_concept].add(item)
+                        except KeyError:
+                            mapping[item.sumo_concept] = {item}
+    #TODO: Remove the fixed assertions/replace with dynamic assertions
+    assert total == 117939
+    assert processed >= 117659 - 2000, 'processed %d, should have processed %d' % (processed, 117659)
+    return mapping
+
+def _wtokenize(line, pos):
+    """ Returns all the tokens of a WordNet data line. """
+    items = line.split(' ')
+    # byte offset in current file
+    synset_offset = int(items.pop(0))
+    lex_filenum = int(items.pop(0))
+    ss_type = SSType(items.pop(0))
+    w_cnt = int(items.pop(0), 16)
+    synset = dict()
+    for i in range(1, w_cnt + 1):
+        word = items.pop(0)
+        if pos == Pos.adj and word[:-1] == ')':
+            listy = word.split('(')
+            syn_marker = listy.pop()[:-1]
+            word = listy.pop()
+        else:
+            syn_marker = None
+        lex_id = int(items.pop(0), 16)
+        synset[i] = (word, syn_marker, lex_id)
+    assert len(synset) == w_cnt
+    p_cnt = int(items.pop(0))
+    ptr_list = list()
+    for i in range(0, p_cnt):
+        pointer_symbol = items.pop(0)
+        synset_offset = int(items.pop(0))
+        p_pos = Pos(items.pop(0))
+        so_ta = items.pop(0)
+        source = int(so_ta[:2], 16)
+        target = int(so_ta[2:], 16)
+        ptr_list.append((pointer_symbol, p_pos, synset_offset, source, target))
+    assert len(ptr_list) == p_cnt, 'line "%s" has %d pointers, but %s only contains %d' % (line, p_cnt, ptr_list, len(ptr_list))
+    frames = None
+    if ss_type == SSType.verb:
+        f_cnt = int(items.pop(0))
+        frames = set()
+        for i in range(0, f_cnt):
+            assert items.pop(0) == '+'
+            f_num = int(items.pop(0))
+            w_num = int(items.pop(0), 16)
+            frames.add((f_num, w_num))
+        assert len(frames) == f_cnt
+    assert items.pop(0) == '|'
+    assert len(items) != 0
+    string = ' '.join(items)
+    assert string != ''
+    items = string.split('&%')
+    assert len(items) >= 2, '"%s": %s should contain at least 2 items, but contains %d' % (line, items, len(items))
+    gloss = items.pop(0).rstrip()
+    sumo_concepts = set()
+    while len(items) > 0:
+        name = items.pop(0)
+        suffix = name[-1:]
+        name = name[:-1]
+        sumo_concepts.add(SUMOConceptWordNetItem(name, suffix, synset_offset, lex_filenum, ss_type, synset, ptr_list, frames, gloss))
+    return sumo_concepts
+
+class SUMOConceptWordNetItem():
+    """ The object returned from _wtokenize containing info on the SUMO-WordNet mapping. """
+    def __init__(self, sumo_concept, suffix, synset_offset, lex_filenum,
+                 ss_type, synset, ptr_list, frames, gloss):
+        self.sumo_concept = sumo_concept
+        self.suffix = suffix
+        self.synset_offset = synset_offset
+        self.lex_filenum = lex_filenum
+        self.ss_type = ss_type
+        self.synset = synset
+        self.ptr_list = ptr_list
+        self.frames = frames
+        self.gloss = gloss
+
+class Pos(Enum):
+    noun = 'n'
+    verb = 'v'
+    adj = 'a'
+    adv = 'r'
+
+class SSType(Enum):
+    noun = 'n'
+    verb = 'v'
+    adj = 'a'
+    adv = 'r'
+    adj_sat = 's'
 
 class AbstractSyntaxTree():
     """ The AbstractSyntaxTree is a node in the abstract syntax tree. The
