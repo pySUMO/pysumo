37b9296827a8e23278b01a87e6e08ea655c29185
Mbatchakwe Kent Harold <uodep@student.kit.edu>
2015-01-22 17:21:56 +0100

    ui: Finish MenuBar and ToolBar for MainWindow.
    Adds Designer files for MainWindow.

--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -9,15 +9,10 @@ This module contains:
 from PySide import QtGui
 from PySide.QtGui import QMainWindow
 from PySide.QtGui import QApplication
-from PySide.QtGui import QMenu
-from PySide.QtGui import QAction
-from PySide.QtGui import QIcon
-from PySide.QtCore import Qt
-from PySide.QtGui import QToolBar
-from ui import gfx
 import sys
+from ui.Designer import MainWindow
 
-class MainWindow(QMainWindow):
+class MainWindow(MainWindow.Ui_mainwindow):
     """ This class is the entry point of the application. It creates the main
     window, initiates all the subsystems and then displays the GUI.  It
     consists of: a main frame with a menu bar, toolbar, status bar and numerous
@@ -31,159 +26,10 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         """ Constructs the main window.  """
-        super(MainWindow, self).__init__()
-        self.resize(680, 364)
-        self.setDockNestingEnabled(True)
-        self.createActions()
-        self.createMenuBar()
-        self.createToolBars()
+        self.mainwindow = QMainWindow()
+        self.setupUi(self.mainwindow)
+        self.mainwindow.show()
         
-    def createMenuBar(self):
-        menubar = self.menuBar()
-        
-        # build the file menu
-        menuFile = QMenu(menubar)
-        menuFile.setTitle("&File")
-        menuFile.addAction(self.newOntologyAction)
-        openOntologyMenu = QMenu(menuFile)
-        openOntologyMenu.setTitle("&Open")
-        openOntologyMenu.addAction(self.openLocalOntologyAction)
-        openOntologyMenu.addAction(self.openRemoteOntologyAction)
-        menuFile.addAction(openOntologyMenu.menuAction())
-        recentOntologiesMenu = QMenu(menuFile)
-        recentOntologiesMenu.setTitle("Recent Ontologies")
-        self.addRecentlyOpenedOntologies(recentOntologiesMenu)
-        recentOntologiesMenu.addSeparator()
-        recentOntologiesMenu.addAction(self.clearHistoryAction)
-        menuFile.addAction(recentOntologiesMenu.menuAction())
-        menuFile.addSeparator()
-        menuFile.addAction(self.saveAction)
-        menuFile.addAction(self.saveAsAction)
-        menuFile.addSeparator()
-        menuFile.addAction(self.printAction)
-        menuFile.addAction(self.quickPrintAction)
-        menuFile.addSeparator()
-        menuFile.addAction(self.propertiesAction)
-        menuFile.addAction(self.revertAction)
-        menuFile.addAction(self.closeAction)
-        menuFile.addSeparator()
-        menuFile.addAction(self.quitAction)
-        
-        # build the edit menu
-        menuEdit = QtGui.QMenu(menubar)
-        menuEdit.setTitle("&Edit")
-        
-        #self.menuOntology = QtGui.QMenu(self.menubar)
-        #self.menuView = QtGui.QMenu(self.menubar)
-        #self.menuTools = QtGui.QMenu(self.menubar)
-        #self.menuHelp = QtGui.QMenu(self.menubar)
-        
-        menubar.addAction(menuFile.menuAction())
-        menubar.addAction(menuEdit.menuAction())
-     
-    def addRecentlyOpenedOntologies(self, parent):
-        """ """
-        
-    def createToolBars(self):
-        fileToolbar = QToolBar(self)
-        fileToolbar.addAction(self.newOntologyAction)
-        fileToolbar.addAction(self.openLocalOntologyAction)
-        fileToolbar.addAction(self.openRemoteOntologyAction)
-        fileToolbar.addSeparator()
-        fileToolbar.addAction(self.saveAction)
-        fileToolbar.addAction(self.printAction)
-        self.addToolBar(fileToolbar)
-        
-    def createActions(self):
-        # New Ontology
-        self.newOntologyAction = QAction(self)
-        self.createAction(self.newOntologyAction, "document-new", "New Ontology", "Ctrl+N", True)
-        
-        # Open Local Ontology
-        self.openLocalOntologyAction = QAction(self)
-        self.createAction(self.openLocalOntologyAction, "document-open", "Local Ontology", "Ctrl+O", True)
-        
-        # Open Remote Ontology
-        self.openRemoteOntologyAction = QAction(self)
-        self.createAction(self.openRemoteOntologyAction, "document-open-remote", "Remote Ontology", "Ctrl+R", True)
-        
-        # Clear History
-        self.clearHistoryAction = QAction(self)
-        self.createAction(self.clearHistoryAction, "edit-clear-history", "Clear History", None, True)
-        
-        # Save 
-        self.saveAction = QAction(self)
-        self.createAction(self.saveAction, "document-save", "Save", "Ctrl+S", True)
-        
-        # Save As
-        self.saveAsAction = QAction(self)
-        self.createAction(self.saveAsAction, "document-save-as", "Save As...", "Ctrl+Shift+S", True)
-        
-        # Print
-        self.printAction = QAction(self)
-        self.createAction(self.printAction, "document-print", "Print...", "Ctrl+P", True)
-        
-        # Quick Print
-        self.quickPrintAction = QAction(self)
-        self.createAction(self.quickPrintAction, "document-quickprint", "Quick Print", "Ctrl+Shift+P", True)
-        
-        # Properties
-        self.propertiesAction = QAction(self)
-        self.createAction(self.propertiesAction, "document-propertiesAction", "Properties...", None, True)
-        
-        # Revert
-        self.revertAction = QAction(self)
-        self.createAction(self.revertAction, "document-revert", "Revert", None, True)
-        
-        # Close
-        self.closeAction = QAction(self)
-        self.createAction(self.closeAction, "document-close", "Close", None, True)
-        
-        # Quit
-        self.quitAction = QAction(self)
-        self.createAction(self.quitAction, "application-exit", "Quit", "Ctrl+Q", True)
-        
-        # Undo
-        self.undoAction = QAction(self)
-        self.createAction(self.undoAction, "edit-undo", "Undo", "Ctrl+Z", True)
-        
-        # Redo
-        self.redoAction = QAction(self)
-        self.createAction(self.redoAction, "edit-redo", "Redo", "Ctrl+Shift+Z", True)
-        
-        # Cut
-        self.cutAction = QAction(self)
-        self.createAction(self.cutAction, "edit-cut", "Cut", "Ctrl+X", True)
-        
-        # Copy
-        self.copyAction = QAction(self)
-        self.createAction(self.copyAction, "edit-copy", "Copy", "Ctrl+C", True)
-        
-        # Paste
-        self.pasteAction = QAction(self)
-        self.createAction(self.pasteAction, "edit-paste", "Paste", "Ctrl+V", True)
-        
-        # Delete
-        self.deleteAction = QAction(self)
-        self.createAction(self.deleteAction, "edit-delete", "Delete", "Delete", True)
-        
-        # Select All
-        self.selectAllAction = QAction(self)
-        self.createAction(self.selectAllAction, "edit-select-all", "Select All", "Ctrl+A", True)
-        
-        # Find 
-        
-    def createAction(self, action, iconName, text, shortcut, visibleInMenu):
-        if iconName != None :
-            icon = QIcon(":/actions/gfx/" + iconName + ".png")
-            action.setIcon(icon)
-        action.setText(text)
-        action.setIconText(text)
-        if shortcut != None :
-            action.setShortcut(shortcut)
-            action.setShortcutContext(Qt.ApplicationShortcut)
-        action.setIconVisibleInMenu(visibleInMenu)
-
     def loadOptions(self):
         """ Loads the options of the main window and all its widgets. """
 
@@ -199,10 +45,8 @@ class MainWindow(QMainWindow):
         
 def main():
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    app.exec_()
-    sys.exit()
+    mw = MainWindow()
+    sys.exit(app.exec_())
     
 if __name__ == '__main__':
     main()
