6d15649156bc91b9b8f73acecef08fcda3c54205
Simon Thelen <urday@student.kit.edu>
2015-01-29 12:50:02 +0100

    ui/*/TextEditor: Add TextEditor designer files.

--- src/ui/Widget/TextEditor.py
+++ src/ui/Widget/TextEditor.py
@@ -3,10 +3,15 @@ widget. It contains the textual representation of the currently loaded
 Ontologies allowing conventional kif editing with features such as syntax
 highlighting and autocompletion.
 """
+import sys
+from PySide.QtGui import QApplication, QMainWindow
+from PySide.QtGui import QTextCharFormat
+from PySide.QtGui import QFont
+from PySide.QtCore import Qt, QRegExp
 
 from ui.Widget.Widget import RWWidget as RWWidget
-
-class TextEditor(RWWidget):
+from ui.Designer.TextEditor import Ui_Form as Ui_Form
+class TextEditor(RWWidget, Ui_Form):
     """ Contains many features of popular text editors adapted for use with
     Ontologies such as syntax highlighting, and autocompletion. One column on
     the left of the text editor contains line numbers and another contains
@@ -26,10 +31,11 @@ class TextEditor(RWWidget):
 
     """
 
-    def __init__(self):
+    def __init__(self,mainwindow):
         """ Initializes the text editor widget. """
-        super(TextEditor, self).__init__()
-        self.syntax_highlighter = QSyntaxHighlighter()
+        super(TextEditor, self).__init__(mainwindow)
+        self.setupUi(self.mw)
+        self.plainTextEdit.show()
 
     def commit(self):
         """ Overrides commit from RWWidget. """
@@ -39,3 +45,12 @@ class TextEditor(RWWidget):
         When the user triggers autocompletion, the text editor uses the
         IndexAbstractor to process the terms which can complete the given
         string. string has to be greater than or equal to 3 characters. """
+
+if __name__ == "__main__":
+    application = QApplication(sys.argv)
+    mainwindow = QMainWindow()
+    x = TextEditor(mainwindow)
+    mainwindow.show()
+    sys.exit(application.exec_())
+    #x.
+    
