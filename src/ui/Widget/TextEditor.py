4a63fe2ba7db87f1f489f3bd29560d3cce34bc79
Simon Thelen <urday@student.kit.edu>
2015-01-29 12:51:43 +0100

    ui/Widget/TextEditor: add basic syntax highlighting

--- src/ui/Widget/TextEditor.py
+++ src/ui/Widget/TextEditor.py
@@ -7,6 +7,7 @@ import sys
 from PySide.QtGui import QApplication, QMainWindow
 from PySide.QtGui import QTextCharFormat
 from PySide.QtGui import QFont
+from PySide.QtGui import QSyntaxHighlighter as QSyntaxHighlighter
 from PySide.QtCore import Qt, QRegExp
 
 from ui.Widget.Widget import RWWidget as RWWidget
@@ -36,6 +37,8 @@ class TextEditor(RWWidget, Ui_Form):
         super(TextEditor, self).__init__(mainwindow)
         self.setupUi(self.mw)
         self.plainTextEdit.show()
+        self.highlighter = SyntaxHighlighter(self.plainTextEdit.document())
+        #self.syntax_highlighter = QSyntaxHighlighter()
 
     def commit(self):
         """ Overrides commit from RWWidget. """
@@ -46,6 +49,40 @@ class TextEditor(RWWidget, Ui_Form):
         IndexAbstractor to process the terms which can complete the given
         string. string has to be greater than or equal to 3 characters. """
 
+class SyntaxHighlightSetting():
+    def __init__(self, expression, font_weight, font_color):
+        self.expression = expression
+        self.font_weight = font_weight
+        self.font_color = font_color
+            
+        self.createFormat()
+    def createFormat(self):
+        self.class_format = QTextCharFormat()
+        self.class_format.setFontWeight(self.font_weight)
+        self.class_format.setForeground(self.font_color)
+            
+    def get_format(self):
+        return self.class_format
+
+class SyntaxHighlighter(QSyntaxHighlighter):
+
+    def __init__(self, document):
+        super(SyntaxHighlighter, self).__init__(document)
+        #Comments
+        self.classes = []
+        self.classes.append(SyntaxHighlightSetting(";.*$", QFont.Bold, Qt.darkMagenta))
+        self.classes.append(SyntaxHighlightSetting("(member|patient|agent|instance|subclass|exists|documentation|part|domain|equal|hasPurpose)\W", QFont.Bold, Qt.darkGreen))
+        self.classes.append(SyntaxHighlightSetting("(and|not|or)\W", QFont.Bold, Qt.black))    
+    def highlightBlock(self, text):
+        for h in self.classes:
+            expression = QRegExp(h.expression)
+            index = expression.indexIn(text)
+            while index >= 0:
+                length = expression.matchedLength()
+                self.setFormat(index, length, h.get_format())
+                index = expression.indexIn(text, index + length)
+  
+    
 if __name__ == "__main__":
     application = QApplication(sys.argv)
     mainwindow = QMainWindow()
