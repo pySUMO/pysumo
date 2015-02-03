91e87965a925bef385209705fa61e5b4a0924ddc
Konstantin Zangerle <info@konstantinzangerle.de>
2015-02-03 17:12:43 +0100

    ui/Designer/HierachyWidget: Add Layout

--- src/ui/Designer/HierarchyWidget.py
+++ src/ui/Designer/HierarchyWidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'HierarchyWidget.ui'
 #
-# Created: Tue Feb  3 15:30:30 2015
+# Created: Tue Feb  3 17:12:34 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,9 +13,17 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(574, 300)
-        self.treeView = QtGui.QTreeView(Form)
-        self.treeView.setGeometry(QtCore.QRect(0, 11, 571, 271))
+        self.widget = QtGui.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(0, 0, 561, 291))
+        self.widget.setObjectName("widget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.verticalLayout.addItem(spacerItem)
+        self.treeView = QtGui.QTreeView(self.widget)
         self.treeView.setObjectName("treeView")
+        self.verticalLayout.addWidget(self.treeView)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
