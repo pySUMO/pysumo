40898e075a98507e907f6ae7279aa2f475dd6c97
Mbatchakwe Kent Harold <uodep@student.kit.edu>
2015-02-06 15:23:10 +0100

    ui/Designer/DocumentationWidget: Fix layout of DocumentationWidget.

--- src/ui/Designer/DocumentationWidget.py
+++ src/ui/Designer/DocumentationWidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'DocumentationWidget.ui'
 #
-# Created: Tue Feb  3 17:09:18 2015
+# Created: Fri Feb  6 15:12:48 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,31 +13,34 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(557, 300)
+        self.gridLayout = QtGui.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.lineEdit = QtGui.QLineEdit(Form)
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 1, 1, 1, 1)
         self.tabWidget = QtGui.QTabWidget(Form)
-        self.tabWidget.setGeometry(QtCore.QRect(10, 40, 531, 241))
         self.tabWidget.setObjectName("tabWidget")
         self.Ontology = QtGui.QWidget()
         self.Ontology.setObjectName("Ontology")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.Ontology)
+        self.horizontalLayout.setObjectName("horizontalLayout")
         self.OntologyText = QtGui.QPlainTextEdit(self.Ontology)
-        self.OntologyText.setGeometry(QtCore.QRect(0, 5, 521, 201))
         self.OntologyText.setObjectName("OntologyText")
+        self.horizontalLayout.addWidget(self.OntologyText)
         self.tabWidget.addTab(self.Ontology, "")
         self.WordNet = QtGui.QWidget()
         self.WordNet.setObjectName("WordNet")
+        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.WordNet)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.WordNetText = QtGui.QPlainTextEdit(self.WordNet)
-        self.WordNetText.setGeometry(QtCore.QRect(0, 5, 521, 210))
         self.WordNetText.setObjectName("WordNetText")
+        self.horizontalLayout_2.addWidget(self.WordNetText)
         self.tabWidget.addTab(self.WordNet, "")
-        self.lineEdit = QtGui.QLineEdit(Form)
-        self.lineEdit.setGeometry(QtCore.QRect(10, 10, 321, 23))
-        self.lineEdit.setText("")
-        self.lineEdit.setObjectName("lineEdit")
-        self.widget = QtGui.QWidget(Form)
-        self.widget.setGeometry(QtCore.QRect(0, 0, 2, 2))
-        self.widget.setObjectName("widget")
-        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.gridLayout.addWidget(self.tabWidget, 2, 1, 1, 1)
 
         self.retranslateUi(Form)
         self.tabWidget.setCurrentIndex(1)
@@ -45,7 +48,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.lineEdit.setPlaceholderText(QtGui.QApplication.translate("Form", "Search", None, QtGui.QApplication.UnicodeUTF8))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.Ontology), QtGui.QApplication.translate("Form", "Ontology", None, QtGui.QApplication.UnicodeUTF8))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.WordNet), QtGui.QApplication.translate("Form", "WordNet", None, QtGui.QApplication.UnicodeUTF8))
-        self.lineEdit.setPlaceholderText(QtGui.QApplication.translate("Form", "Search", None, QtGui.QApplication.UnicodeUTF8))
 
