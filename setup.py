52cd79d7045a69ff5073af7ed14e9ed774de7a39
Simon Thelen <urday@student.kit.edu>
2015-02-12 20:45:24 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='pySUMO',
+    version='0.0.0a1',
+    description='A graphical IDE for Ontologies written in SUO-Kif',
+    long_description='A graphical IDE for Ontologies written in SUO-Kif',
+    url='',
+    author='',
+    author_email='',
+    license='',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'License :: ',
+                 'Programming Language :: Python :: 3.4',
+                ],
+    keywords='SUMO Ontologies SUO-Kif',
+    package_dir={'':'src'},
+    packages=['pysumo', 'pysumo.logger', 'pySUMOQt', 'pySUMOQt.Designer', 'pySUMOQt.Widget'],
+    install_requires=['pyside'],
+    extras_require={'test' : ['pytest']},
+    data_files=[('data', ['data/Merge.kif', 'data/MILO.kif']),
+                ('data/wordnet', [''.join(['data/wordnet/sdata.', x]) for x in
+                                  ['adj', 'adv', 'noun', 'verb']]),],
+    entry_points={'gui_scripts': ['pySUMOQt = pySUMOQt.MainWindow:main']},
+)
+
