7e123926edd9b3d7be7953bc1023ceb84965f47c
Simon Thelen <urday@student.kit.edu>
2015-02-12 17:14:55 +0100

    test/lib/syntaxcontroller: Add asserts to check where equality breaks if it breaks.

--- test/lib/syntaxcontroller.py
+++ test/lib/syntaxcontroller.py
@@ -1,5 +1,8 @@
 import unittest
 
+from copy import deepcopy
+from pickle import dumps
+
 from pysumo.syntaxcontroller import *
 from pysumo.indexabstractor import IndexAbstractor
 from pysumo.parser import kifparse, AbstractSyntaxTree
@@ -21,8 +24,22 @@ class syntaxTestCase(unittest.TestCase):
 
     def test1RedundantAdd(self):
         self.syntaxcontroller.add_ontology(self.sumo)
-        old_ast = self.syntaxcontroller.index.root
+        old_ast = deepcopy(self.syntaxcontroller.index.root)
         self.syntaxcontroller.add_ontology(self.sumo)
+        x = deepcopy(old_ast.children[0])
+        y = deepcopy(self.syntaxcontroller.index.root.children[0])
+        self.assertEqual(dumps(x), dumps(y))
+        self.assertEqual(old_ast.name, self.syntaxcontroller.index.root.name)
+        self.assertEqual(old_ast.element_type, self.syntaxcontroller.index.root.element_type)
+        self.assertEqual(old_ast.ontology, self.syntaxcontroller.index.root.ontology)
+        self.assertEqual(old_ast.line, self.syntaxcontroller.index.root.line)
+        self.assertEqual(old_ast.children[0].name, self.syntaxcontroller.index.root.children[0].name)
+        self.assertEqual(old_ast.children[0].element_type, self.syntaxcontroller.index.root.children[0].element_type)
+        self.assertEqual(old_ast.children[0].ontology, self.syntaxcontroller.index.root.children[0].ontology)
+        self.assertEqual(old_ast.children[0].line, self.syntaxcontroller.index.root.children[0].line)
+        self.assertEqual(old_ast.children[0].children, self.syntaxcontroller.index.root.children[0].children)
+        self.assertEqual(old_ast.children[0], self.syntaxcontroller.index.root.children[0])
+        self.assertListEqual(old_ast.children, self.syntaxcontroller.index.root.children)
         self.assertEqual(old_ast, self.syntaxcontroller.index.root)
 
     def test2WipeRemove(self):
@@ -33,12 +50,15 @@ class syntaxTestCase(unittest.TestCase):
         self.assertEqual(self.syntaxcontroller.index.root, AbstractSyntaxTree(None))
 
     def test3AnotherAdd(self):
+        self.maxDiff = None
         self.syntaxcontroller.add_ontology(self.sumo)
         old_ast = self.syntaxcontroller.index.root
         self.syntaxcontroller.add_ontology(self.milo)
         self.assertNotEqual(self.syntaxcontroller.index.root, old_ast)
         sterm = self.syntaxcontroller.index.search('raNgesubclass')
-        self.assertDictEqual(sterm, {self.sumo: ['( instance rangeSubclass BinaryPredicate )', '( instance rangeSubclass AsymmetricRelation )', '( domain rangeSubclass 1 Function )', '( domainSubclass rangeSubclass 2 SetOrClass )', '( documentation rangeSubclass EnglishLanguage "(&%rangeSubclass ?FUNCTION ?CLASS) means thatall of the values assigned by ?FUNCTION are &%subclasses of ?CLASS." )']})
+        print(sterm[self.sumo])
+        print(['( instance rangeSubclass BinaryPredicate )', '( instance rangeSubclass AsymmetricRelation )', '( domain rangeSubclass 1 Function )', '( domainSubclass rangeSubclass 2 SetOrClass )', '( documentation rangeSubclass EnglishLanguage "(&%rangeSubclass ?FUNCTION ?CLASS) means thatall of the values assigned by ?FUNCTION are &%subclasses of ?CLASS." )'])
+        self.assertListEqual(sterm[self.sumo], ['( instance rangeSubclass BinaryPredicate )', '( instance rangeSubclass AsymmetricRelation )', '( domain rangeSubclass 1 Function )', '( domainSubclass rangeSubclass 2 SetOrClass )', '( documentation rangeSubclass EnglishLanguage "(&%rangeSubclass ?FUNCTION ?CLASS) means thatall of the values assigned by ?FUNCTION are &%subclasses of ?CLASS." )'])
         mterm = self.syntaxcontroller.index.search('organISMRemains')
         self.assertNotEqual(mterm, None)
 
