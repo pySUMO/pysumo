1bd3e655c7e6dff92156b77e202d2d443f8dbc14
Simon Thelen <urday@student.kit.edu>
2015-02-14 00:32:09 +0100

    pySUMOQt/DocumentationWidget: Fix search result parsing.

--- src/pySUMOQt/Widget/DocumentationWidget.py
+++ src/pySUMOQt/Widget/DocumentationWidget.py
@@ -5,22 +5,21 @@ print the definition for a selected term
 
 from PySide.QtCore import Slot
 from PySide.QtGui import QApplication, QMainWindow
-import sys
 
 from pySUMOQt.Designer.DocumentationWidget import Ui_Form
 from .Widget import RWidget
 
 
 class DocumentationWidget(RWidget, Ui_Form):
-
     """ The DocumentationWidget displays relevant information about the
     current Ontology, and offers a search interface to WordNet. """
+
 
     def __init__(self, mainwindow):
         """ Initializes the DocumentationWidget. """
         super(DocumentationWidget, self).__init__(mainwindow)
         self.setupUi(self.mw)
-        self.lineEdit.editingFinished.connect(self.search)
+        self.lineEdit.returnPressed.connect(self.search)
 
     @Slot()
     def search(self):
@@ -29,16 +28,16 @@ class DocumentationWidget(RWidget, Ui_Form):
         searchOntology = self.getIndexAbstractor().search(
             self.lineEdit.text())
 
-        self.OntologyText.setPlainText(k + v for (k, v) in searchOntology)
+        self.OntologyText.setPlainText(searchToText(searchOntology))
         searchWordnet = self.getIndexAbstractor().wordnet_locate(
             self.lineEdit.text())
-        self.WordNetText.setPlainText(k + v for (k, v) in searchWordnet)
+        self.WordNetText.setPlainText(locateToText(searchWordnet))
 
-        # print(self.lineEdit.text())
+def searchToText(search_results):
+    string = ''
+    for ontology in search_results.keys():
+        string = ''.join([string, str(ontology), ':\n', '\n'.join(search_results[ontology]), '\n'])
+    return string
 
-if __name__ == "__main__":
-    application = QApplication(sys.argv)
-    mainwindow = QMainWindow()
-    x = DocumentationWidget(mainwindow)
-    mainwindow.show()
-    sys.exit(application.exec_())
+def locateToText(locate_results):
+    return '\n'.join(locate_results)
