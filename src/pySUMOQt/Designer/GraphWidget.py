3bacd7398fc9f1653abf827c057dc0cd06e6212f
Konstantin Zangerle <info@konstantinzangerle.de>
2015-02-20 15:08:31 +0100

    pySUMOQt/Designer/GraphWidget: Add search line to GraphWidget.

--- src/pySUMOQt/Designer/GraphWidget.py
+++ src/pySUMOQt/Designer/GraphWidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'GraphWidget.ui'
 #
-# Created: Wed Feb 18 11:49:26 2015
+# Created: Fri Feb 20 15:08:23 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,34 +13,41 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(889, 680)
-        self.widget = QtGui.QWidget(Form)
-        self.widget.setGeometry(QtCore.QRect(3, 0, 881, 671))
-        self.widget.setObjectName("widget")
-        self.gridLayout = QtGui.QGridLayout(self.widget)
+        self.layoutWidget = QtGui.QWidget(Form)
+        self.layoutWidget.setGeometry(QtCore.QRect(3, 0, 881, 671))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.gridLayout = QtGui.QGridLayout(self.layoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
-        self.pushButton = QtGui.QPushButton(self.widget)
+        self.lineEdit = QtGui.QLineEdit(self.layoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 0, 4, 1, 1)
+        self.pushButton = QtGui.QPushButton(self.layoutWidget)
         self.pushButton.setObjectName("pushButton")
         self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.gridLayout.addItem(spacerItem, 0, 1, 1, 1)
-        self.relations = QtGui.QComboBox(self.widget)
+        self.relations = QtGui.QComboBox(self.layoutWidget)
         self.relations.setObjectName("relations")
         self.gridLayout.addWidget(self.relations, 0, 2, 1, 1)
         spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.gridLayout.addItem(spacerItem1, 0, 3, 1, 1)
-        self.doubleSpinBox = QtGui.QDoubleSpinBox(self.widget)
+        self.gridLayout.addItem(spacerItem1, 0, 5, 1, 1)
+        self.doubleSpinBox = QtGui.QDoubleSpinBox(self.layoutWidget)
+        self.doubleSpinBox.setMinimum(0.01)
         self.doubleSpinBox.setMaximum(5.0)
         self.doubleSpinBox.setSingleStep(0.1)
         self.doubleSpinBox.setProperty("value", 1.0)
         self.doubleSpinBox.setObjectName("doubleSpinBox")
-        self.gridLayout.addWidget(self.doubleSpinBox, 0, 4, 1, 1)
-        self.comboBox = QtGui.QComboBox(self.widget)
+        self.gridLayout.addWidget(self.doubleSpinBox, 0, 6, 1, 1)
+        self.comboBox = QtGui.QComboBox(self.layoutWidget)
         self.comboBox.setObjectName("comboBox")
-        self.gridLayout.addWidget(self.comboBox, 0, 5, 1, 1)
-        self.graphicsView = QtGui.QGraphicsView(self.widget)
+        self.gridLayout.addWidget(self.comboBox, 0, 7, 1, 1)
+        self.graphicsView = QtGui.QGraphicsView(self.layoutWidget)
         self.graphicsView.setObjectName("graphicsView")
-        self.gridLayout.addWidget(self.graphicsView, 1, 0, 1, 6)
+        self.gridLayout.addWidget(self.graphicsView, 1, 0, 1, 8)
+        self.label = QtGui.QLabel(self.layoutWidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 3, 1, 1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -48,4 +55,5 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton.setText(QtGui.QApplication.translate("Form", "+", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Form", "Search for node:", None, QtGui.QApplication.UnicodeUTF8))
 
