4476bef62e5de8052dca7b7e103084d4b02260b6
Simon Thelen <urday@student.kit.edu>
2015-02-24 21:37:59 +0100

    pySUMOQt/Settings: Add LayoutManager class.
    Saves and restores pySUMO qt component states.

--- src/pySUMOQt/Settings.py
+++ src/pySUMOQt/Settings.py
@@ -25,6 +25,144 @@ class WSettings(QSettings):
 
         """
         pass
+    
+class LayoutManager(QSettings):
+    
+    def __init__(self, MainWindow):
+        super(LayoutManager, self).__init__("user-layout.ini", QSettings.IniFormat)
+        self.mainwindow = MainWindow
+    
+    def saveLayout(self):
+        self.savePositionState(self.mainwindow)
+        self.saveSizeState(self.mainwindow)
+        self.setValue("mainWindow/state", self.mainwindow.saveState())
+        self.saveStatusBarState()
+        actions = self.mainwindow.menuTextEditorWidgets.actions()
+        count = len(actions)
+        self.setValue("TextEditorWidgets/count", count)
+        actions = self.mainwindow.menuDocumentationWidgets.actions()
+        count = len(actions)
+        self.setValue("DocumentationWidgets/count", count)
+        actions = self.mainwindow.menuHierarchyWidgets.actions()
+        count = len(actions)
+        self.setValue("HierarchyWidgets/count", count)
+        self.saveRecentOntologyHistory()
+    
+    def restoreLayout(self):
+        self.mainwindow.restoreState(self.value("mainWindow/state"))
+        self.restoreSizeState(self.mainwindow)
+        self.restorePositionState(self.mainwindow)
+        self.restoreStatusBarState()
+        # restore Text Editor Widgets
+        self.restoreTextEditorWidgets()
+        # restore Documentation Widgets
+        self.restoreDocumentationWidgets()
+        # restore Hierarchy Widgets
+        self.restoreHierarchyWidgets()
+        self.restoreRecentOntologyHistory()
+        
+    def saveRecentOntologyHistory(self):
+        pass
+
+    def restoreRecentOntologyHistory(self):
+        pass
+    
+    def restoreTextEditorWidgets(self):
+        textEditorWidgetsCount = self.value("TextEditorWidgets/count")
+        if textEditorWidgetsCount == None:
+            textEditorWidgetsCount = 0
+        textEditorWidgetsCount = int(textEditorWidgetsCount)
+        count = 0
+        while count < textEditorWidgetsCount:
+            widget = self.mainwindow.createPySumoWidget("TextEditorWidget", self.mainwindow.menuTextEditorWidgets)
+            self.mainwindow.addOrRestoreWidget(widget, self.mainwindow.menuTextEditorWidgets)
+            count = count + 1
+            
+    def restoreDocumentationWidgets(self):
+        documentationWidgetsCount = self.value("DocumentationWidgets/count")
+        if documentationWidgetsCount == None:
+            documentationWidgetsCount = 0
+        documentationWidgetsCount = int(documentationWidgetsCount)
+        count = 0 
+        while count < documentationWidgetsCount:
+            widget = self.mainwindow.createPySumoWidget("DocumentationWidget", self.mainwindow.menuDocumentationWidgets)
+            self.mainwindow.addOrRestoreWidget(widget, self.mainwindow.menuDocumentationWidgets)
+            count = count + 1
+
+    def restoreHierarchyWidgets(self):
+        hierarchyWidgetsCount = self.value("HierarchyWidgets/count")
+        if hierarchyWidgetsCount == None:
+            hierarchyWidgetsCount = 0
+        hierarchyWidgetsCount = int(hierarchyWidgetsCount)
+        count = 0 
+        while count < hierarchyWidgetsCount:
+            widget = self.mainwindow.createPySumoWidget("HierarchyWidget", self.mainwindow.menuHierarchyWidgets)
+            self.mainwindow.addOrRestoreWidget(widget, self.mainwindow.menuHierarchyWidgets)
+            count = count + 1
+
+    def saveVisibilityState(self, qItem):
+        objName = qItem.objectName()
+        self.setValue(objName + "/visible", qItem.isVisible())
+
+    def restoreVisibilityState(self, qItem, qAction=None):
+        objName = qItem.objectName()
+        visible = self.value(objName + "/visible")
+        visible = str(visible).lower()
+        if visible == "false":
+            visible = False
+        elif visible == "true":
+            visible = True
+        else:
+            visible = None
+        # if not visible :
+        #   qAction.triggered.emit()
+        #  qAction.setChecked(False)
+        if visible != None:
+            if qAction != None:
+                qAction.setChecked(visible)
+            qItem.setVisible(visible)
+
+    def savePositionState(self, qItem):
+        objName = qItem.objectName()
+        pos = qItem.pos()
+        xPos = pos.x()
+        yPos = pos.y()
+        self.setValue(objName + "/x", xPos)
+        self.setValue(objName + "/y", yPos)
+
+    def restorePositionState(self, qItem):
+        objName = qItem.objectName()
+        xPos = self.value(objName + "/x")
+        yPos = self.value(objName + "/y")
+        if xPos == None or yPos == None:
+            return
+        xPos = int(xPos)
+        yPos = int(yPos)
+        qItem.move(xPos, yPos)
+
+    def saveSizeState(self, qItem):
+        objName = qItem.objectName()
+        size = qItem.size()
+        width = size.width()
+        height = size.height()
+        self.setValue(objName + "/width", width)
+        self.setValue(objName + "/height", height)
+
+    def restoreSizeState(self, qItem):
+        objName = qItem.objectName()
+        width = self.value(objName + "/width")
+        height = self.value(objName + "/height")
+        if width == None or height == None:
+            return
+        width = int(width)
+        height = int(height)
+        qItem.resize(width, height)
+
+    def saveStatusBarState(self):
+        self.saveVisibilityState(self.mainwindow.statusBar)
+
+    def restoreStatusBarState(self):
+        self.restoreVisibilityState(self.mainwindow.statusBar, self.mainwindow.actionStatusbar)
 
 class PluginManager():
     """ The PluginManager handles all loadabel plugins. At startup it loads all
