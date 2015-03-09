8114f9d0994152861f3c6334bcdd04d7ddde16ea
Konstantin Zangerle <info@konstantinzangerle.de>
2015-03-09 15:17:06 +0100

    Expand Testcase

--- test/gui/graphwidget.py
+++ test/gui/graphwidget.py
@@ -8,8 +8,19 @@ import shutil
 Steps:
 1. Open pySUMO
 2. Open GraphWidget
+3. Open Merge.kif
 3. Select instance on Variant selector
-4. Add a node in Graph Widget
+4. Select a root node
+5. Select a depth (1)
+6. Open a new ontology to write temporary content to.
+7. Add a node "bla" in Graph Widget
+8. Add a node "bla2"
+9. Add a node "bla" (error)
+10. Add a relation instance between "bla" and "bla2"
+11. Change scale
+12. Choose a valid selector (there was an messagebox)
+13. Undo
+14. Redo
 """
 if __name__ == "__main__":
     tmpdir = mkdtemp()
