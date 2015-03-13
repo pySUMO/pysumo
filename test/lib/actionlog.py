538cbbe98967cb8f9b16855c3b23b9fcd6c3d1d5
Simon Thelen <urday@student.kit.edu>
2015-03-13 22:38:08 +0100

    test/lib/actionlog: Swap assert with self.assertEqual()

--- test/lib/actionlog.py
+++ test/lib/actionlog.py
@@ -90,21 +90,21 @@ class actionLogTestCase(unittest.TestCase):
         with open('/'.join([CORRUPTDIR, '000.kif']), 'r+b') as kif:
             new = BytesIO(kif.read())
         self.add_and_ok(new)
-        assert len(self.al.actionlog) == 2
+        self.assertEqual(len(self.al.actionlog), 2)
         self.al.log_io.flush_write_queues()
-        assert os.listdir(self.tmpdir) == ['test']
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test']))) == sorted(['current', 'redo', 'undo'])
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))) == sorted(['000', '001'])
+        self.assertEqual(os.listdir(self.tmpdir), ['test'])
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test']))), sorted(['current', 'redo', 'undo']))
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))), sorted(['000', '001']))
 
     def test3Undo(self):
         self.add_and_ok(SUMO)
         with open('/'.join([CORRUPTDIR, '000.kif']), 'r+b') as kif:
             new = BytesIO(kif.read())
         self.add_and_ok(new)
-        assert self.al.undo().getvalue() == SUMO.getvalue()
+        self.assertEqual(self.al.undo().getvalue(), SUMO.getvalue())
         self.al.log_io.flush_write_queues()
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))) == sorted(['000'])
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))) == sorted(['000'])
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))), sorted(['000']))
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))), sorted(['000']))
 
     def test4Redo(self):
         self.add_and_ok(SUMO)
@@ -112,10 +112,10 @@ class actionLogTestCase(unittest.TestCase):
             new = BytesIO(kif.read())
         self.add_and_ok(new)
         self.al.undo()
-        assert self.al.redo().getvalue() == new.getvalue()
+        self.assertEqual(self.al.redo().getvalue(), new.getvalue())
         self.al.log_io.flush_write_queues()
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))) == sorted(['000', '001'])
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))) == []
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))), sorted(['000', '001']))
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))), [])
 
     def test5SizeFlush(self):
         self.add_and_ok(SUMO)
@@ -124,7 +124,7 @@ class actionLogTestCase(unittest.TestCase):
             with open('/'.join([CORRUPTDIR, kif]), 'r+b') as mkif:
                 new = BytesIO(mkif.read())
             self.add_and_ok(new)
-        assert len(self.al.actionlog) == 10
+        self.assertEqual(len(self.al.actionlog), 10)
         assert self.al.log_io.uwrite_queue == [], 'uwrite_queue is %s, but should be []' % self.al.log_io.uwrite_queue
         assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))) == sorted(['%03d' % x for x in range(0, 10)]), 'undo contains %s, but should contain %s' % (sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))), sorted(['%03d' % x for x in range(0, 10)]))
 
@@ -135,14 +135,14 @@ class actionLogTestCase(unittest.TestCase):
                 new = BytesIO(mkif.read())
             num = self.al.queue_log(new)
         qnum, diff = self.al.queue
-        assert num == qnum
-        assert diff is new
+        self.assertEqual(num, qnum)
+        self.assertEqual(diff, new)
         self.al.ok_log_item(num)
-        assert len(self.al.actionlog) == 2
+        self.assertEqual(len(self.al.actionlog), 2)
         self.al.log_io.flush_write_queues()
-        assert os.listdir(self.tmpdir) == ['test']
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test']))) == sorted(['current', 'redo', 'undo'])
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))) == sorted(['000', '001'])
+        self.assertEqual(os.listdir(self.tmpdir), ['test'])
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test']))), sorted(['current', 'redo', 'undo']))
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))), sorted(['000', '001']))
 
     def test7ExcessiveUndo(self):
         self.add_and_ok(SUMO)
@@ -160,7 +160,7 @@ class actionLogTestCase(unittest.TestCase):
             with open('/'.join([CORRUPTDIR, kif]), 'r+b') as mkif:
                 new = BytesIO(mkif.read())
             self.add_and_ok(new)
-        assert self.al.redo().getvalue() == new.getvalue()
+        self.assertEqual(self.al.redo().getvalue(), new.getvalue())
 
     def test9HammerTime(self):
         self.add_and_ok(SUMO)
@@ -174,23 +174,23 @@ class actionLogTestCase(unittest.TestCase):
         for i in range(0, 20):
             self.al.undo()
             self.al.redo()
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))) == redoes
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))), redoes)
         assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))) == undoes, 'undo is %s, but should be %s' % (sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))), undoes)
-        assert self.al.current.getvalue() == curr.getvalue()
+        self.assertEqual(self.al.current.getvalue(), curr.getvalue())
         for i in range(0, 17):
             self.al.undo()
         for i in range(0, 13):
             self.al.redo()
         self.al.undo()
         self.al.redo()
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))) == sorted(['%03d' % x for x in range(0, 4)])
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))), sorted(['%03d' % x for x in range(0, 4)]))
         for kif in os.listdir(CORRUPTDIR)[50:]:
             with open('/'.join([CORRUPTDIR, kif]), 'r+b') as mkif:
                 new = BytesIO(mkif.read())
             self.add_and_ok(new)
-        assert self.al.current.getvalue() == new.getvalue()
+        self.assertEqual(self.al.current.getvalue(), new.getvalue())
         assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))) == sorted(['%03d' % x for x in range(0, 110)]), 'undo is %s, but should be %s' % (sorted(os.listdir('/'.join([self.tmpdir, 'test', 'undo']))), sorted(['%03d' % x for x in range(0, 110)]))
-        assert sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))) == []
+        self.assertEqual(sorted(os.listdir('/'.join([self.tmpdir, 'test', 'redo']))), [])
 
     def test10FalseOk(self):
         num = self.al.queue_log(SUMO)
