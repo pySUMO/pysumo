2c4a350df659c9a7a5e3f6c60834c204798fbe06
Mbatchakwe Kent Harold <uodep@student.kit.edu>
2015-03-15 19:03:52 +0100

    completed the test case for the main window.

--- test/gui/mainwindow.py
+++ test/gui/mainwindow.py
@@ -13,15 +13,22 @@ Steps:
 1. Open pySUMO
 2. Open a new Ontology named "Mondial"
 3. Open a remote ontology named "Mondial" at location: http://sigmakee.cvs.sourceforge.net/viewvc/sigmakee/KBs/mondial.kif
-4. Override the old version of the file Mondial by confirming the dialog.
+4. Close the ontology named "Mondial" with save and reopen it as local file.
 5. Add Text Editor Widget
 6. Add Documentation Widget
 7. Add Graph Widget
 8. Add Hierarchy Widget
 9. Reorder Widgets
 10. Make a print preview of the ontology "Mondial" from the Text Editor Widget
+10a. Delete the Text Editor Widget
 11. Make a print preview of the ontology "Mondial" from the Graph Widget
-12. Quit pySUMO and save changes on file Mondial.kif
+11a. Add a node and a relation in Graph widget
+11b. Delete the Graph Widget
+12. Try to update the ontology "Mondial"
+13. Open ontology properties dialog for "Mondial" and then close it.
+14. Open Option Dialog and then close it
+15. Revert the ontology "Mondial"
+16. Quit pySUMO
 """
 if __name__ == "__main__":
     tmpdir = mkdtemp()
