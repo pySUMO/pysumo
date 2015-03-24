aba673d4cffad16044282f7b5b346c190f57a050
Konstantin Zangerle <info@konstantinzangerle.de>
2015-03-24 14:19:53 +0100

    Open two windows to see if settings were saved

--- test/gui/settings.py
+++ test/gui/settings.py
@@ -1,6 +1,8 @@
 """ Test case for the GraphWidget"""
 from tempfile import mkdtemp
 from pySUMOQt import MainWindow
+from PySide.QtGui import QApplication
+import sys
 import pysumo
 import shutil
 
@@ -19,7 +21,12 @@ Steps:
 if __name__ == "__main__":
     tmpdir = mkdtemp()
     pysumo.CONFIG_PATH = tmpdir
-    MainWindow.main()
-    MainWindow.main()
+    app = QApplication(sys.argv)
+    x1 = MainWindow.MainWindow()
+    app.setActiveWindow(x1)
+    app.exec_()
+    x2 = MainWindow.MainWindow()
+    app.setActiveWindow(x2)
+    app.exec_()
     shutil.rmtree(tmpdir, ignore_errors=True)
 
